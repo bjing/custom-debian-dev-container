a1c837acb3d38d28fe1b066f884a99a3b3afb051
Brian Jing <brian.jing.dev@gmail.com>
2023-09-13 00:04:03 +1000

    Fix a bug where coc-settings.json couldn't be copied

--- scripts/setup_vim.sh
+++ scripts/setup_vim.sh
@@ -2,10 +2,14 @@
 
 sudo apt update && sudo apt install -y nodejs yarn
 
+# Put .vimrc file in th correct location
 cp .vimrc $HOME/
-cp coc-settings.json $HOME/.vim/
 sudo chown $USERNAME:$USERNAME $HOME/.vimrc
-sudo chown $USERNAME:$USERNAME $HOME/.vim/coc-settings.json
 
 # Install Vim plugins
 vim +PlugInstall +qall
+
+# Put coc-settings.json in the correct location. 
+# This can only be done after the above step which creates the ~/.vim direcory
+cp coc-settings.json $HOME/.vim/
+sudo chown $USERNAME:$USERNAME $HOME/.vim/coc-settings.json
